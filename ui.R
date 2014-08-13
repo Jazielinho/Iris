90e2bcc15d6be80d24d8c3732f181827b90ed3bf
Jazielinho <jahazielponce@gmail.com>
2014-08-13 17:42:23 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,8 +4,11 @@ shinyUI(fluidPage(
             sidebarPanel(
                   p("This app show descriptive analysis from data table 'iris'"),
                   br(),
+                  p("The following application shows a descriptive analysis of the data first used by fisher."),
+                  p("You select the variables in the iris data and the main panel shows plots, and summary data."),
                   selectInput("variable","Select Variable",
                               names(iris)),
+                  br(),
                   br(),
                   p("The code is is:",
                   a("this link",
